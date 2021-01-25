107fd3e3a85bdcb186a0986d8915a6e3f70d7cd1
zhangjie <zhangjie@ccbtrust.com.cn>
2021-01-25 12:18:34 +0800

    opti

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "index.html": "d0960d525d88265f75295758792ffb24",
 "/": "d0960d525d88265f75295758792ffb24",
-"main.dart.js": "e2e84f31178074a057c3a77de4c5c299",
+"main.dart.js": "cb515aeb30f1ed96a773ff867c6af19b",
 "assets/LICENSE": "a1d2628e23afc715cb6490550cb35a31",
 "assets/images/top.png": "f2df608073ce878aca2c91c45247a492",
 "assets/images/bg.png": "329d60a371ed04c8c803bb9f012316e4",
